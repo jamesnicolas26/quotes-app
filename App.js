8cc7dd9b84f6959dc9a5603509bd12ca28029206
jamesnicolas26 <58098815+jamesnicolas26@users.noreply.github.com>
2020-12-07 14:16:49 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,8 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import QuoteOfTheDayScreen from './src/screens/QuoteOfTheDayScreen';
+import FavoriteQuotesScreen from './src/screens/FavoriteQuotesScreen';
+import Login from './src/screens/LoginScreen';
 
-export default function App() {
-  return <Home />;
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Login" 
+                    component={Login} 
+                    options={{ 
+                    title: 'Login',
+                    headerTintColor: 'white',
+                    headerTitleStyle: { alignSelf: 'center' },
+                    headerStyle: { backgroundColor: '#9f0cfa' }
+                   }}
+        />
+        <Stack.Screen name="Home" component={HomeScreen} 
+                  options={{ 
+                    title: 'Home',
+                    headerTintColor: 'white',
+                    headerTitleStyle: { alignSelf: 'center' },
+                    headerStyle: { backgroundColor: '#9f0cfa' }
+                   }}
+        />
+        <Stack.Screen name="InspireMe" component={QuoteOfTheDayScreen} 
+                  options={{ 
+                    title: 'Quote of the Day',
+                    headerTintColor: 'white',
+                    headerStyle: { backgroundColor: '#9f0cfa' }
+                   }}
+        />
+        <Stack.Screen name="FavQuote" component={FavoriteQuotesScreen} 
+                  options={{ 
+                    title: 'Favorite Quotes',
+                    headerTintColor: 'white',
+                    headerStyle: { backgroundColor: '#9f0cfa' }
+                   }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default App;
